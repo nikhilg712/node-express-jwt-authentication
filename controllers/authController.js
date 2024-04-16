b798dab0c06a2bb617abea0f7f10ae9205c23b0b
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-16 23:06:51 +0530

    logout functionality and checking user

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,15 @@ const jwt = require("jsonwebtoken");
 const handleErrors = (err) => {
   let error = { email: "", password: "" };
   console.log(err.message, err.code);
+
+  if(err.message === 'incorrect mail'){
+    error.email = 'This email is not registered'
+  }
+
+  if(err.message === 'incorrect password'){
+    error.password = 'This password is incorrect'
+  }
+
   if (err.code === 11000) {
     error.email = "Email already taken";
     return error;
@@ -44,9 +53,16 @@ module.exports.login_post = async (req, res) => {
   const {email,password} = (req.body);
   try {
     const user = await User.login(email,password);
+    const token = createToken(user._id)
+    res.cookie('jwt',token,{httpOnly: true, maxAge:maxAge*1000})
     res.status(200).json({user:user._id})
   } catch (err) {
-    res.status(400).json({})
+    const errors = handleErrors(err);
+    res.status(400).json({errors})
   }
-
 };
+
+module.exports.logout_get = (req,res)=>{
+  res.cookie('jwt','',{maxAge:1})
+  res.redirect('/')
+}
